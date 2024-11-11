3e8bdc0fc9dc828680209647cf05915dfe3617d8
DennyRodrigues <denny.rodrigues.carmo@gmail.com>
2024-11-11 14:48:56 -0300

    Add contact section

--- src/app/components/contact/index.tsx
+++ src/app/components/contact/index.tsx
@@ -1,13 +1,34 @@
 import React from 'react'
 import LogoIcon from '/public/logo.svg';
+import FacebookIcon from '/public/facebook.svg';
+import TwitterIcon from '/public/twitter.svg';
+import InstagramIcon from '/public/instagram.svg';
+import Link from 'next/link';
 
 export default function index() {
   return (
-    <section>
-      <div className='mr-auto ml-auto'>
-        <LogoIcon className="fill-darkPurple"/>
+    <section className='pb-16'>
+      <div className='mb-14'>
+        <LogoIcon className="fill-darkPurple mr-auto ml-auto" />
       </div>
-      index
+
+      <div className="flex items-center justify-center gap-7">
+        <Link href="#" className="group">
+          <FacebookIcon className="
+                fill-davyGrey
+          group-hover:fill-darkPurple group-active:fill-darkPurple" />
+        </Link>
+        <button className="group">
+          <TwitterIcon className="
+                fill-davyGrey
+          group-hover:fill-darkPurple group-active:fill-darkPurple" />
+        </button>
+        <button className="group">
+          <InstagramIcon className="
+          fill-davyGrey
+          group-hover:fill-darkPurple group-active:fill-darkPurple" />
+        </button>
+      </div>
 
     </section>
   )
